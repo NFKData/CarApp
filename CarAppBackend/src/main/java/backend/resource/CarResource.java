2ddc13e5767cf6fcd8ab89c4c0c60c1f23c63b80
Guillermo Miralles Campillo <guillermo.miralles.campillo@everis.com>
2019-05-22 09:20:22 +0200

    Resource working correctly

--- CarAppBackend/src/main/java/backend/resource/CarResource.java
+++ CarAppBackend/src/main/java/backend/resource/CarResource.java
@@ -1,5 +1,7 @@
 package backend.resource;
 
+import java.util.List;
+
 import javax.ejb.EJB;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
@@ -9,6 +11,7 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.GenericEntity;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
@@ -27,7 +30,7 @@ public class CarResource {
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response getCars() {
 		return Response.status(Status.OK)
-				.entity(carService.getAllCars())
+				.entity(new GenericEntity<List<Car>>(carService.getAllCars()) {})
 				.build();
 	}
 
