49d107d034d5dd9e499d886acb70d99115b6c9ca
Guillermo Miralles Campillo <guillermo.miralles.campillo@everis.com>
2019-06-07 14:52:49 +0200

    Auth filter only verifies if JWT is valid

--- CarAppBackend/src/main/java/com/backend/filter/AuthFilter.java
+++ CarAppBackend/src/main/java/com/backend/filter/AuthFilter.java
@@ -1,9 +1,6 @@
 package com.backend.filter;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Base64;
-import java.util.List;
 
 import javax.annotation.Priority;
 import javax.ws.rs.Priorities;
@@ -13,43 +10,36 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.ext.Provider;
 
-import com.auth0.client.auth.AuthAPI;
-import com.auth0.exception.Auth0Exception;
-import com.auth0.json.auth.CreatedUser;
-import com.auth0.json.auth.TokenHolder;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.JWTVerifier;
 
 @Provider
-@Priority(Priorities.AUTHENTICATION)
+@Priority(Priorities.AUTHORIZATION)
 public class AuthFilter implements ContainerRequestFilter {
 
-	private static final String SCOPE = "openid email";
-	private static final String AUTHORIZATION = "authorization";
+	private static final String ERROR_401 = "Request must include \"Authorization\" header.";
+	private static final String ERROR_403 = "Auth token not valid";
+	private static final String JWT_SECRET = "jwt.secret";
 	private static final String BEARER = "Bearer ";
-	private static final String DOMAIN = "everis-carapp.eu.auth0.com";
-	private static final String CLIENT_ID = "rrDYabEzebUPsoDSsRdlxmd5MESQPAck";
-	private static final String CLIENT_SECRET = "bexcssRgKirgeoGHPdePPk59YfpazPbQjvKYnos126do9U3J8Uz1PAsMhUTpVZZI";
-	private static final String API_AUDIENCE = "https://everis-carapp.eu.auth0.com/api/v2/";
-	private static final String CONNECTION = "Username-Password-Authentication";
 
 	@Override
 	public void filter(ContainerRequestContext requestContext) throws IOException {
 		String authHeader = requestContext.getHeaderString("Authorization");
 		if (authHeader == null) {
 			requestContext.abortWith(Response.status(Status.UNAUTHORIZED)
-					.entity("Request must include \"Authorization\" header.").build());
+					.entity(ERROR_401).build());
 		}
 	
-		String[] authData = new String(Base64.getDecoder().decode((authHeader.split("\\s+"))[1].getBytes())).split(":");
-		AuthAPI authApi = new AuthAPI(DOMAIN, CLIENT_ID, CLIENT_SECRET);
-		try {
-		TokenHolder tokenHolderLogin = authApi.login(authData[0], authData[1]).setAudience(API_AUDIENCE)
-				.setScope(SCOPE).execute();
-		List<String> authHeaders = new ArrayList<>();
-		authHeaders.add(BEARER + tokenHolderLogin.getAccessToken());
-		requestContext.getHeaders().put(AUTHORIZATION, authHeaders);
-		} catch(Auth0Exception ex) {
-			CreatedUser createdUser = authApi.signUp(authData[0], authData[1], CONNECTION).execute();
-			requestContext.abortWith(Response.status(Status.UNAUTHORIZED).entity("A verification email should be sent to " + createdUser.getEmail()).build());
+		authHeader = authHeader.substring(BEARER.length());
+		Algorithm alg = Algorithm.HMAC256(System.getProperty(JWT_SECRET));
+		JWTVerifier verifier = JWT.require(alg).build();
+		try  {
+			verifier.verify(authHeader);
+		} catch(JWTVerificationException e) {
+			requestContext.abortWith(Response.status(Status.FORBIDDEN)
+					.entity(ERROR_403).build());
 		}
 	}
 
